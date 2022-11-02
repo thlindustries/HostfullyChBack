7067e3b2746548521575f30395aa797445cc306b
thlindustries <thlindustries23@gmail.com>
2022-11-02 15:07:28 -0300

    Chore -> removing fake delay

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const fakeDelay = (req, res, next) => { setTimeout(next, 2000) }
 const port = process.env.PORT || 3001;
 
-server.use(fakeDelay);
 server.use(middlewares);
 server.use(router);
 
